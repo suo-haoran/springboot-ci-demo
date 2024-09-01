7bf67a32bf3fb06ca58996605eb0ee3d98710c8d
Suo Haoran <suohaoran99@gmail.com>
2024-09-01 16:05:35 +0800

    Standardize endpoints & server port

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/todo")
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
     @Autowired
